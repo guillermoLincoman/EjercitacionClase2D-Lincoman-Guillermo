e56ffc0ac0a28f1d1a7a8923fe4f9a14fc69ef4c
lincomanGuillermo <holasoyguishe@gmail.com>
2022-08-22 22:46:41 -0300

    Clase 1

--- Clase1/Ejercicios/Program.cs
+++ Clase1/Ejercicios/Program.cs
@@ -6,7 +6,34 @@ namespace Ejercicios
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numMax = int.MinValue;
+            int numMin = int.MaxValue;
+            int acumNumeros = 0;
+            int cantNumeros = 5;
+            int i = 0;
+            int numIngresado;
+            while (i != cantNumeros)
+            {
+                Console.WriteLine("Ingrese un numero");
+                if (int.TryParse(Console.ReadLine(), out numIngresado))
+                {
+                    if (numIngresado < numMin)
+                    {
+                        numMin = numIngresado;
+                    }
+                    if (numIngresado > numMax)
+                    {
+                        numMax = numIngresado;
+                    }
+                    acumNumeros += numIngresado;
+                    i++;
+                }else
+                {
+                    Console.WriteLine("Numero no valido");
+                }
+            }
+            float promNumeros = (float)acumNumeros / cantNumeros;
+            Console.WriteLine($"\nNumeros Maximo {numMax}\nNumeros Minimo {numMin}\nNumeros promedio {promNumeros}\n");
         }
     }
 }
