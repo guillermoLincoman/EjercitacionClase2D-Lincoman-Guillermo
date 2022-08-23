2b775ca4be1bb802ca14c6aff28b3c17809b99df
lincomanGuillermo <holasoyguishe@gmail.com>
2022-08-22 21:21:22 -0300

    nuevos

---
+++ Clase1/Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombres = new string[3];
+            nombres[0] = "Juan";
+            nombres[1] = "Ruban";
+            nombres[2] = "Nacho";
+
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                Console.WriteLine(nombres[i]);
+            }
+
+
+            string[] paises = { "Brasil", "Peru", "Chile" };
+
+            foreach (var loQueGuardo in paises)
+            {
+                Console.WriteLine(loQueGuardo);
+            }
+        }
+    }
+}
---
+++ Clase1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int edad;
+            string edadAux;
+            /*Console.WriteLine($"{nombre} tenes {edad} años");
+            Console.WriteLine($"{0} tenes {1} años", nombre, edad);*/
+
+            Console.WriteLine("Nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Edad");
+            edadAux = Console.ReadLine();
+
+            //para escribir flotantes
+            float precio = 43.3245f;
+            //No hace falta la f
+            double doble = 35.3;
+            //Es mas preciso que double
+            decimal numeroDecimal = 26.4M;
+            //Toma el primer dato que se le ingresa y se convierte a ese tipo
+            var edad2 = 4;
+
+            if (int.TryParse(edadAux, out edad))
+            {
+                Console.WriteLine($"{nombre} tu edad es {edad}");
+            }
+            else {
+                Console.WriteLine($"{nombre}, escribi una edad valida gato");
+            }
+            Console.WriteLine($"Asi mascarizo un flotante {precio:C2}");
+
+        }
+    }
+}
